4b7a4bd1b5f248c4792f23704a314d62a8e3a2a9
brainconnect93 <brainconnect0@gmail.com>
2022-09-30 01:11:04 +0100

    Remove the validation before running the test

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,5 @@
 class Movie < ApplicationRecord
 
-  validates :title, presence: true, length: { in: 6..25 }
-  validates :director, presence: true, length: { in: 5..20 } 
-  validates :rate, presence: true, length: { in: 1..10 }, numericality: { only_integer: true }
   
+
 end
