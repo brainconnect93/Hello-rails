ee7f455993e07482675d3e9c360e116f34f64138
brainconnect93 <brainconnect0@gmail.com>
2022-09-30 01:12:52 +0100

    add the validation and run the test

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ApplicationRecord
 
-  
+  validates :title, presence: true, length: { in: 6..25 }
+  validates :director, presence: true, length: { in: 5..20 } 
+  validates :rate, presence: true, length: { in: 1..10 }, numericality: { only_integer: true }
 
 end
